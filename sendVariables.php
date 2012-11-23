b5f6fad8c17ced61b1e7233629d5a20ed8ac73c8
Denno D M Secqtinstien <erlsindia@erlsindia.co.in>
2012-11-23 19:03:03 +0530

    files
    adding new files to this repository

---
+++ sendVariables.php
@@ -0,0 +1,12 @@
+<?php
+$impressionNumber		= $_POST['impressionNumber'];
+$imageURL		= $_POST['imageURL'];
+$clickNumber		= $_POST['clickNumber'];
+$clickNumberTrue		= $_POST['clickNumberTrue'];
+$clickImpressionTrue		= $_POST['clickImpressionTrue'];
+$izpis = 'test '.$izpis .'<br>'. $impressionNumber;
+
+echo $izpis ;
+
+
+?>
